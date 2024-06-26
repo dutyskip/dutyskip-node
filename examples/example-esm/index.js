614bf872ce7c62e5b2b282c46df3ad3208a7973a
Max Stein <maxwell.stein@gmail.com>
2024-06-26 16:46:01 -0700

    updates examples

--- examples/example-esm/index.js
+++ examples/example-esm/index.js
@@ -4,6 +4,6 @@ import 'dotenv/config'
 const dutyskip = new Dutyskip(process.env?.DUTYSKIP_API_KEY)
 
 void (async () => {
-  const results = await dutyskip.search('chapstick', 'canada')
+  const results = await dutyskip.hs.search('chapstick', 'canada')
   console.log(results)
 })()
--- examples/example-ts/src/index.ts
+++ examples/example-ts/src/index.ts
@@ -3,6 +3,6 @@ import Dutyskip from 'dutyskip'
 const dutyskip = new Dutyskip(process.env?.DUTYSKIP_API_KEY as string)
 
 void (async () => {
-  const results = await dutyskip.search('chapstick', 'canada')
+  const results = await dutyskip.hs.search('chapstick', 'canada')
   console.log(results)
 })()
