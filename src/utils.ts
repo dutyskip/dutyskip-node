37380adf0b5af889259190d2cc6e4dfd513d8c97
Max Stein <maxwell.stein@gmail.com>
2024-06-28 11:59:55 -0700

    refactor: changes method format

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,3 @@
-export function buildUrl(protocol: string, host: string, config?: { port?: string }) {
+export function buildUrl(protocol: string, host: string, config?: { port?: string }): string {
   return config?.port ? `${protocol}${host}:${config?.port}` : `${protocol}${host}`
 }
