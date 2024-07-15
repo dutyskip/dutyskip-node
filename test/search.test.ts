d47c524d5ff5a9951ae00c715e70bf2a99f9aab0
Max Stein <maxwell.stein@gmail.com>
2024-07-15 15:19:15 -0700

    adds vitest and configures search test

---
+++ test/search.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import Dutyskip from '../dist'
+const dutyskip = new Dutyskip('4b7f6118-555b-4dba-b0ec-7d2025745778')
+
+test('hs search canada', async () => {
+  const { items } = await dutyskip.hs.search('chapstick', 'canada') || {}
+  const { code } = items[0]
+  expect(code).toBe('3304.99.90.90')
+})
+
+test('hs search usa', async () => {
+  const { items } = await dutyskip.hs.search('chapstick', 'usa') || {}
+  const { code } = items[0]
+  expect(code).toBe('3304.99.50.00')
+})
